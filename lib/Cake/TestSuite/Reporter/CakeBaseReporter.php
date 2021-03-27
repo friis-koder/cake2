1c3b305d5d4b8520de4a7f0093538633a57c3fe2
Dennis Friis Raaschou <df@cego.dk>
2021-03-27 12:25:23 +0100

    More refactor.

--- lib/Cake/TestSuite/Reporter/CakeBaseReporter.php
+++ lib/Cake/TestSuite/Reporter/CakeBaseReporter.php
@@ -15,223 +15,259 @@
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 
-if (!class_exists('PHPUnit_TextUI_ResultPrinter')) {
-	require_once 'PHPUnit/TextUI/ResultPrinter.php';
-}
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestResult;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Framework\Warning;
+use PHPUnit\TextUI\ResultPrinter;
 
 /**
  * CakeBaseReporter contains common reporting features used in the CakePHP Test suite
  *
  * @package       Cake.TestSuite.Reporter
  */
-class CakeBaseReporter extends PHPUnit_TextUI_ResultPrinter {
+class CakeBaseReporter implements ResultPrinter
+{
+    /**
+     * Headers sent
+     *
+     * @var bool
+     */
+    protected $_headerSent = false;
 
-/**
- * Headers sent
- *
- * @var bool
- */
-	protected $_headerSent = false;
+    /**
+     * Array of request parameters. Usually parsed GET params.
+     *
+     * @var array
+     */
+    public $params = array();
 
-/**
- * Array of request parameters. Usually parsed GET params.
- *
- * @var array
- */
-	public $params = array();
+    /**
+     * Character set for the output of test reporting.
+     *
+     * @var string
+     */
+    protected $_characterSet;
 
-/**
- * Character set for the output of test reporting.
- *
- * @var string
- */
-	protected $_characterSet;
+    /**
+     * Number of assertions
+     *
+     * @var int
+     */
+    protected $numAssertions;
 
-/**
- * Does nothing yet. The first output will
- * be sent on the first test start.
- *
- * ### Params
- *
- * - show_passes - Should passes be shown
- * - plugin - Plugin test being run?
- * - core - Core test being run.
- * - case - The case being run
- * - codeCoverage - Whether the case/group being run is being code covered.
- *
- * @param string $charset The character set to output with. Defaults to UTF-8
- * @param array $params Array of request parameters the reporter should use. See above.
- */
-	public function __construct($charset = 'utf-8', $params = array()) {
-		if (!$charset) {
-			$charset = 'utf-8';
-		}
-		$this->_characterSet = $charset;
-		$this->params = $params;
-	}
+    /**
+     * Does nothing yet. The first output will
+     * be sent on the first test start.
+     *
+     * ### Params
+     *
+     * - show_passes - Should passes be shown
+     * - plugin - Plugin test being run?
+     * - core - Core test being run.
+     * - case - The case being run
+     * - codeCoverage - Whether the case/group being run is being code covered.
+     *
+     * @param string $charset The character set to output with. Defaults to UTF-8
+     * @param array $params Array of request parameters the reporter should use. See above.
+     */
+    public function __construct($charset = 'utf-8', $params = array())
+    {
+        if (!$charset) {
+            $charset = 'utf-8';
+        }
 
-/**
- * Retrieves a list of test cases from the active Manager class,
- * displaying it in the correct format for the reporter subclass
- *
- * @return mixed
- */
-	public function testCaseList() {
-		$testList = CakeTestLoader::generateTestList($this->params);
-		return $testList;
-	}
+        $this->_characterSet = $charset;
+        $this->params = $params;
+    }
 
-/**
- * Paints the start of the response from the test suite.
- * Used to paint things like head elements in an html page.
- *
- * @return void
- */
-	public function paintDocumentStart() {
-	}
+    /**
+     * Retrieves a list of test cases from the active Manager class,
+     * displaying it in the correct format for the reporter subclass
+     *
+     * @return mixed
+     */
+    public function testCaseList()
+    {
+        return CakeTestLoader::generateTestList($this->params);
+    }
 
-/**
- * Paints the end of the response from the test suite.
- * Used to paint things like </body> in an html page.
- *
- * @return void
- */
-	public function paintDocumentEnd() {
-	}
+    /**
+     * Paints the start of the response from the test suite.
+     * Used to paint things like head elements in an html page.
+     */
+    public function paintDocumentStart()
+    {
+    }
 
-/**
- * Paint a list of test sets, core, app, and plugin test sets
- * available.
- *
- * @return void
- */
-	public function paintTestMenu() {
-	}
+    /**
+     * Paints the end of the response from the test suite.
+     * Used to paint things like </body> in an html page.
+     */
+    public function paintDocumentEnd()
+    {
+    }
 
-/**
- * Get the baseUrl if one is available.
- *
- * @return string The base URL for the request.
- */
-	public function baseUrl() {
-		if (!empty($_SERVER['PHP_SELF'])) {
-			return $_SERVER['PHP_SELF'];
-		}
-		return '';
-	}
+    /**
+     * Paint a list of test sets, core, app, and plugin test sets
+     * available.
+     */
+    public function paintTestMenu()
+    {
+    }
 
-/**
- * Print result
- *
- * @param PHPUnit_Framework_TestResult $result The result object
- * @return void
- */
-	public function printResult(PHPUnit_Framework_TestResult $result) {
-		$this->paintFooter($result);
-	}
+    /**
+     * Get the baseUrl if one is available.
+     *
+     * @return string The base URL for the request.
+     */
+    public function baseUrl(): string
+    {
+        if (!empty($_SERVER['PHP_SELF'])) {
+            return $_SERVER['PHP_SELF'];
+        }
 
-/**
- * Paint result
- *
- * @param PHPUnit_Framework_TestResult $result The result object
- * @return void
- */
-	public function paintResult(PHPUnit_Framework_TestResult $result) {
-		$this->paintFooter($result);
-	}
+        return '';
+    }
 
-/**
- * An error occurred.
- *
- * @param PHPUnit_Framework_Test $test The test to add an error for.
- * @param Exception $e The exception object to add.
- * @param float $time The current time.
- * @return void
- */
-	public function addError(PHPUnit_Framework_Test $test, Exception $e, $time) {
-		$this->paintException($e, $test);
-	}
+    /**
+     * Print result
+     *
+     * @param TestResult $result The result object
+     */
+    public function printResult(TestResult $result): void
+    {
+        $this->paintFooter($result);
+    }
 
-/**
- * A failure occurred.
- *
- * @param PHPUnit_Framework_Test $test The test that failed
- * @param PHPUnit_Framework_AssertionFailedError $e The assertion that failed.
- * @param float $time The current time.
- * @return void
- */
-	public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time) {
-		$this->paintFail($e, $test);
-	}
+    /**
+     * Paint result
+     *
+     * @param TestResult $result The result object
+     */
+    public function paintResult(TestResult $result)
+    {
+        $this->paintFooter($result);
+    }
 
-/**
- * Incomplete test.
- *
- * @param PHPUnit_Framework_Test $test The test that was incomplete.
- * @param Exception $e The incomplete exception
- * @param float $time The current time.
- * @return void
- */
-	public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time) {
-		$this->paintSkip($e, $test);
-	}
+    /**
+     * An error occurred.
+     *
+     * @param Test $test The test to add an error for.
+     * @param Throwable $throwable
+     * @param float $time The current time.
+     */
+    public function addError(Test $test, Throwable $throwable, float $time): void
+    {
+        $this->paintException($throwable, $test);
+    }
 
-/**
- * Skipped test.
- *
- * @param PHPUnit_Framework_Test $test The test that failed.
- * @param Exception $e The skip object.
- * @param float $time The current time.
- * @return void
- */
-	public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time) {
-		$this->paintSkip($e, $test);
-	}
+    /**
+     * A failure occurred.
+     *
+     * @param Test $test The test that failed
+     * @param AssertionFailedError $error The assertion that failed.
+     * @param float $time The current time.
+     */
+    public function addFailure(Test $test, AssertionFailedError $error, float $time): void
+    {
+        $this->paintFail($error, $test);
+    }
 
-/**
- * A test suite started.
- *
- * @param PHPUnit_Framework_TestSuite $suite The suite to start
- * @return void
- */
-	public function startTestSuite(PHPUnit_Framework_TestSuite $suite) {
-		if (!$this->_headerSent) {
-			echo $this->paintHeader();
-		}
-		echo __d('cake_dev', 'Running  %s', $suite->getName()) . "\n";
-	}
+    /**
+     * Incomplete test.
+     *
+     * @param Test $test The test that was incomplete.
+     * @param Throwable $throwable
+     * @param float $time The current time.
+     */
+    public function addIncompleteTest(Test $test, Throwable $throwable, float $time): void
+    {
+        $this->paintSkip($throwable, $test);
+    }
 
-/**
- * A test suite ended.
- *
- * @param PHPUnit_Framework_TestSuite $suite The suite that ended.
- * @return void
- */
-	public function endTestSuite(PHPUnit_Framework_TestSuite $suite) {
-	}
+    /**
+     * Skipped test.
+     *
+     * @param Test $test The test that failed.
+     * @param Throwable $throwable
+     * @param float $time The current time.
+     */
+    public function addSkippedTest(Test $test, Throwable $throwable, float $time): void
+    {
+        $this->paintSkip($throwable, $test);
+    }
 
-/**
- * A test started.
- *
- * @param PHPUnit_Framework_Test $test The test that started.
- * @return void
- */
-	public function startTest(PHPUnit_Framework_Test $test) {
-	}
+    /**
+     * A test suite started.
+     *
+     * @param TestSuite $suite The suite to start
+     */
+    public function startTestSuite(TestSuite $suite): void
+    {
+        if (!$this->_headerSent) {
+            echo $this->paintHeader();
+        }
 
-/**
- * A test ended.
- *
- * @param PHPUnit_Framework_Test $test The test that ended
- * @param float $time The current time.
- * @return void
- */
-	public function endTest(PHPUnit_Framework_Test $test, $time) {
-		$this->numAssertions += $test->getNumAssertions();
-		if ($test->hasFailed()) {
-			return;
-		}
-		$this->paintPass($test, $time);
-	}
+        echo __d('cake_dev', 'Running  %s', $suite->getName()) . "\n";
+    }
 
+    /**
+     * A test suite ended.
+     *
+     * @param TestSuite $suite The suite that ended.
+     */
+    public function endTestSuite(TestSuite $suite): void
+    {
+    }
+
+    /**
+     * A test started.
+     *
+     * @param Test $test The test that started.
+     */
+    public function startTest(Test $test): void
+    {
+        $test->run();
+    }
+
+    /**
+     * A test ended.
+     *
+     * @param Test $test The test that ended
+     * @param float $time The current time.
+     */
+    public function endTest(Test $test, float $time): void
+    {
+        $this->numAssertions += $test->getNumAssertions();
+
+        $this->paintPass($test, $time);
+    }
+
+    /**
+     * write
+     *
+     * @param string $buffer
+     */
+    public function write(string $buffer): void
+    {
+        // TODO: Implement write() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addWarning(Test $test, Warning $e, float $time): void
+    {
+        // TODO: Implement addWarning() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addRiskyTest(Test $test, Throwable $t, float $time): void
+    {
+        // TODO: Implement addRiskyTest() method.
+    }
 }
